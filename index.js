ab0f57d86b4be17dd84c1cd99457e9d3ca49c9c0
Aeron Avery <contact@aeronavery.com>
2023-04-12 15:23:18 -0700

    Added license information Moved diff-match-patch to its own directory

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+/**
+ * Roland
+ * Copyright 2023 Tin Hat Studios
+ * https://github.com/tinhatstudios/roland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const chokidar = require('chokidar');
-const diff_match_patch = require('./diff-match-patch.js');
+const diff_match_patch = require('./lib/diff-match-patch.js');
 const express = require('express');
 const fs = require('fs');
 const http = require('http');
